f817b0fe6d6843b007d8fc0d0530fde9c69bd538
Arek <arek314arek@gmail.com>
2018-10-06 17:03:56 +0200

    Added AI possibility

--- src/main/java/com/tuco/draughts/DraughtsState.java
+++ src/main/java/com/tuco/draughts/DraughtsState.java
@@ -2,27 +2,28 @@ package com.tuco.draughts;
 
 import com.tuco.draughts.board.Board;
 import com.tuco.draughts.board.util.BoardCreator;
-import com.tuco.draughts.movement.Movement;
-import com.tuco.draughts.movement.MovementContainer;
+import com.tuco.draughts.board.util.Coordinate;
 import com.tuco.draughts.movement.MovementHelper;
+import com.tuco.draughts.movement.util.Movement;
+import com.tuco.draughts.movement.util.MovementCoder;
+import com.tuco.draughts.movement.util.MovementContainer;
+import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sac.game.GameState;
 import sac.game.GameStateImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DraughtsState extends GameStateImpl {
 
     private final static Logger LOG = LogManager.getLogger(DraughtsState.class);
 
+    @Getter
     private final Board board;
-    private final MovementHelper movementHelper;
 
-    static {
-//        TODO: set heuristic
-//        setHFunction(new MyHeuristic());
-    }
+    private final MovementHelper movementHelper;
 
     public DraughtsState(BoardCreator boardCreator) {
         this.board = new Board(boardCreator);
@@ -33,7 +34,7 @@ public class DraughtsState extends GameStateImpl {
     private DraughtsState(DraughtsState parent) {
         this.board = new Board(parent.board);
         this.movementHelper = new MovementHelper(board);
-        setMaximizingTurnNow(!parent.maximizingTurnNow);
+        setMaximizingTurnNow(parent.maximizingTurnNow);
     }
 
     private void endTurn() {
@@ -56,8 +57,17 @@ public class DraughtsState extends GameStateImpl {
 
     @Override
     public List<GameState> generateChildren() {
-//        TODO:
-        return null;
+        List<GameState> children = new ArrayList<>();
+        MovementContainer possibleMoves = generatePossibleMoves();
+        possibleMoves.getMovements().forEach(movement -> children.add(generateChild(movement)));
+        return children;
+    }
+
+    private GameState generateChild(Movement movement) {
+        DraughtsState child = new DraughtsState(this);
+        child.makeMove(movement);
+        child.setMoveName(MovementCoder.code(movement));
+        return child;
     }
 
     @Override
